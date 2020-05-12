f1b7040bdabbb7e2f8894504a88067e0ddb56a9c
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-12 12:30:05 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,7 +3,7 @@ from src.song import Song
 
 def test_exercise(capsys):
     garden = Song("In The Garden", 10910)
-    print("The song " + garden.name() + " has a length of " + str(garden.length()) + " seconds.")
+    print("The song " + garden.name + " has a length of " + str(garden.length) + " seconds.")
 
     out, err = capsys.readouterr()
     assert out == "The song In The Garden has a length of 10910 seconds.\n"
